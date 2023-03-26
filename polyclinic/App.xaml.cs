b72af7aabcde044821331297ebaeaccca0d1b05a
Aliaksei <alx.leonenko@gmail.com>
2023-03-26 19:30:06 +0300

    Добавьте файлы проекта.

---
+++ polyclinic/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace polyclinic;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ polyclinic/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace polyclinic;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
