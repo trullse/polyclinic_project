cadf0239bfe0ba4ae4605854314eb4e638f011bc
Aliaksei <alx.leonenko@gmail.com>
2023-04-23 00:47:57 +0300

    add IRepository and IUnitOfWork interfaces

---
+++ polyclinic.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using polyclinic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyclinic.Domain.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Appointment> AppointmentRepository { get; }
+        IRepository<Client> ClientRepository { get; }
+        IRepository<Doctor> DoctorRepository { get; }
+        public Task RemoveDatbaseAsync();
+        public Task CreateDatabaseAsync();
+        public Task SaveAllAsync();
+    }
+}
