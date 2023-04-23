f5f79a148577c5b47d1c0d0f015b0ec09ae56c92
Aliaksei <alx.leonenko@gmail.com>
2023-04-23 23:26:36 +0300

    add DbContext

---
+++ polyclinic.Persistence/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace polyclinic.Persistence.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Нужно ли? https://metanit.com/sharp/entityframeworkcore/2.2.php
+        // public DbSet<User> Users { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
