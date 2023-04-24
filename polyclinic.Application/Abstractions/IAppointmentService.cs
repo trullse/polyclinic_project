945a43e7b517bd13ce36c9ba7ec880e1254093f3
Aliaksei <alx.leonenko@gmail.com>
2023-04-24 16:26:56 +0300

    add services abstractions

---
+++ polyclinic.Application/Abstractions/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using polyclinic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyclinic.Application.Abstractions
+{
+    public interface IAppointmentService : IBaseService<Appointment>
+    {
+    }
+}
---
+++ polyclinic.Application/Abstractions/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using polyclinic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyclinic.Application.Abstractions
+{
+    public interface IBaseService<T> where T : Entity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task<T> DeleteAsync(int id);
+    }
+}
