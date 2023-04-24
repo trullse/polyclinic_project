a5bf19b5bae6305ee36634d6eb5a246923120116
Aliaksei <alx.leonenko@gmail.com>
2023-04-24 19:50:37 +0300

    add community toolkit and registrate services

--- polyclinic.UI/MauiProgram.cs
+++ polyclinic.UI/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using polyclinic.Application.Abstractions;
+using polyclinic.Application.Services;
+using polyclinic.Domain.Abstractions;
+using polyclinic.Persistence.Repository;
 
 namespace polyclinic.UI
 {
@@ -9,17 +14,34 @@ namespace polyclinic.UI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            SetupServices(builder.Services);
+
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        private static void SetupServices(IServiceCollection services)
+        {
+            // Services
+            services.AddSingleton<IUnitOfWork, FakeUnitOfWork>();
+            services.AddSingleton<IAppointmentService, AppointmentService>();
+            services.AddSingleton<IClientService, ClientService>();
+            services.AddSingleton<IDoctorService, DoctorService>();
+
+            // Views
+
+            // ViewModels
+
         }
     }
 }
