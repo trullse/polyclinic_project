30dd90c056841755e650d7637be98df4b41d0987
Aliaksei <alx.leonenko@gmail.com>
2023-04-25 16:51:11 +0300

    add appointments DateToColorConverter

---
+++ polyclinic.UI/ValueConverters/DateToColorValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyclinic.UI.ValueConverters
+{
+    internal class DateToColorValueConverter : IValueConverter
+    {
+        private readonly Color passed = Color.FromArgb("218359");
+        private readonly Color future = Colors.Transparent;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            
+            if ((DateTime)value <= DateTime.Now)
+                return passed;
+            return future;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
