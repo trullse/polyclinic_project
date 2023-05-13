6cbfc36dccd078f3c32b5307111a8eff5bd90667
Aliaksei <alx.leonenko@gmail.com>
2023-05-14 00:04:01 +0300

    add redesigned appointments

---
+++ polyclinic.UI/ValueConverters/StatusValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static polyclinic.Domain.Entities.Appointment;
+
+namespace polyclinic.UI.ValueConverters
+{
+    public class StatusValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (Status)value;
+            switch (status)
+            {
+                case Status.Approved:
+                    return "Approved";
+                case Status.Ended:
+                    return "Ended";
+                case Status.ToPay:
+                    return "Payment required";
+                case Status.Missed:
+                    return "Missed";
+                case Status.Booked:
+                    return "Booked";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ polyclinic.UI/ValueConverters/TimeValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyclinic.UI.ValueConverters
+{
+    public class TimeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+            return date.ToShortTimeString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
