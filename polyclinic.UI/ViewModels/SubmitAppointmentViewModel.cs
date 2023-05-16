b46e12acd20ec1da7e9d99f34020bd1d8ef39fcb
Aliaksei <alx.leonenko@gmail.com>
2023-05-17 00:26:02 +0300

    add appointment submit info

--- polyclinic.UI/ViewModels/SubmitAppointmentViewModel.cs
+++ polyclinic.UI/ViewModels/SubmitAppointmentViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using polyclinic.Application.Abstractions;
+using polyclinic.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace polyclinic.UI.ViewModels
 {
+    [QueryProperty(nameof(AddDoctor), "AddDoctor")]
+    [QueryProperty(nameof(AddDate), "AddDate")]
+    [QueryProperty(nameof(AddClient), "AddClient")]
     public partial class SubmitAppointmentViewModel : ObservableObject
     {
+        IAppointmentService _appointmentService;
+        public SubmitAppointmentViewModel(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [ObservableProperty]
+        Doctor addDoctor;
+        [ObservableProperty]
+        Client addClient;
+        [ObservableProperty]
+        DateTime addDate;
     }
 }
